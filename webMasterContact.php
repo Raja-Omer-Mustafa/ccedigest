0421142e4208579336b46873eccc568688833d18
ahmer-naheed-247 <ahmer.naheed.247@gmail.com>
2021-08-17 16:49:30 +0500

    php-mailer-integrated

---
+++ webMasterContact.php
@@ -0,0 +1,41 @@
+<?php
+    use PHPMailer\PHPMailer\PHPMailer;
+
+    if (isset($_POST['name']) && isset($_POST['email'])) {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $subject = $_POST['subject'];
+        $body = $_POST['body'];
+
+        require_once "PHPMailer/PHPMailer.php";
+        require_once "PHPMailer/SMTP.php";
+        require_once "PHPMailer/Exception.php";
+
+        $mail = new PHPMailer();
+
+         //SMTP Settings
+        $mail->isSMTP();
+        $mail->Host = "mail.ccedigest.com.pk";
+        $mail->SMTPAuth = true;
+        $mail->Username = "webmaster@ccedigest.com.pk"; //enter you email address
+        $mail->Password = 'iQb3O@B6l?Yn'; //enter you email password
+        $mail->Port = 465;
+        $mail->SMTPSecure = "ssl";
+        //Email Settings
+        $mail->isHTML(true);
+        $mail->setFrom($email, $name);
+        $mail->addAddress("webmaster@ccedigest.com.pk"); //enter you email address
+        $mail->Subject = ("$email ($subject)");
+        $mail->Body = $body;
+
+        if ($mail->send()) {
+            $status = "success";
+            $response = "Email is sent!";
+        } else {
+            $status = "failed";
+            $response = "Something is wrong: <br><br>" . $mail->ErrorInfo;
+        }
+
+        exit(json_encode(array("status" => $status, "response" => $response)));
+    }
+?>
